19944f0a2becd0064674bcc9401970da68001cba
Mihail Borzenko <borzenkomihail@yahoo.com>
2019-06-05 09:22:46 +0300

    Add react-router and basic page

---
+++ src/pages/welcomePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class WelcomePage extends React.Component {
+    render() {
+        return <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores perspiciatis sunt distinctio, voluptatum, cum voluptate aliquam voluptatem doloremque cumque nulla eaque eveniet quis blanditiis. Adipisci error vitae itaque dignissimos quibusdam?</h1>
+    }
+}
+
+export default WelcomePage;
